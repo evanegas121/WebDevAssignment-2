d604b112472526ce8c5db3eed8aaf8bd72dec09b
evanegas121 <Elizabeth.Vanegas11@myhunter.cuny.edu>
2021-10-08 00:00:18 -0400

    include edit

--- assignment2.js
+++ assignment2.js
@@ -1,6 +1,5 @@
 
-// const arr = [1,2];
-// FOR EACH // executes a provided function once per array element.
+// FOR EACH // 
 Array.prototype.myEach = function(callbackFn) {
     for(let i =0 ; i < this.length; i++){
         if (this[i] == undefined) continue;
@@ -8,32 +7,18 @@ Array.prototype.myEach = function(callbackFn) {
         callbackFn(this[i],i, this);
     }
 };
-//Test
-// console.log("myEach: ")
-// arr.myEach((x,i) => console.log(x,i));
 
-// console.log("forEach: ")
-// arr.forEach((x,i) => console.log(x,i))
-
-//const array1 = [1, 4, 9, 16];
 
 // // MAP // 
-// creates a new array populated with the results of calling a provided function on every element in the calling array.
 Array.prototype.myMap = function(callbackFn) {
     const map1 = [];
     for(let i =0 ; i < this.length; i++){
         if (this[i] == undefined) continue;
-        const map1 = (this[i],i, this)
+        map1[map1.length] = (this[i],i, this)
         callbackFn(map1);
     }
 };
 
-//Test
-// array1.map((x,i) => console.log(x*2,i));
-// // const map1 = array1.map(x => x * 2);
-// // console.log(map1);
-// console.log("Map: ")
-// array1.map((x,i) => console.log(x*2,i))
 
 // // FILTER //
 //The filter() method creates a new array with all elements that pass the test implemented by the provided function
@@ -47,14 +32,6 @@ Array.prototype.myFilter = function(callbackFn) {
     } 
     return filter;
 };
-// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-// const result = words.filter(word => word.length > 6);
-// console.log(result);
-
-// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-// const result = words.myFilter(word => word.length > 6);
-// console.log(result);
-//expected output: Array ["exuberant", "destruction", "present"]
 
 // // SOME //
  Array.prototype.mySome = function(callbackFn) {
@@ -66,18 +43,7 @@ Array.prototype.myFilter = function(callbackFn) {
     }
     return false;
 
-
  };
-//  const fruits = ['apple', 'banana', 'mango', 'guava'];
-
-//  function checkAvailability(arr, val) {
-//    return arr.mySome(function(arrVal) {
-//      return val === arrVal;
-//    });
-//  }
- 
-//  console.log(checkAvailability(fruits, 'kela'))  // false
-//  console.log(checkAvailability(fruits, 'banana'))
 
 // // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
@@ -89,49 +55,69 @@ Array.prototype.myEvery = function(callbackFn) {
     }
     return true;
 };
-// const isBelowThreshold = (currentValue) => currentValue >= 40;
-// const array1 = [40, 50, 70, 10, 80, 90];
-// console.log(array1.myEvery(isBelowThreshold)); 
+
 
 // // REDUCE //
 Array.prototype.myReduce = function(callbackFn,value) {
     for (let i =0 ; i < this.length; i++) {
         if (value == undefined) {value = this[i];}
-        else { value = callbackFn.call(undefined, value, this[i],i, this);}
+        else { value = callbackFn.call(undefined, value,this[i],i,this);}
       }
       return value;
 };
 
 // // INCLUDES //
-Array.prototype.myIncludes = function(callbackFn) {
-    for(let i =0 ; i < this.length; i++){
-        if (this[i] == undefined) continue;
-        if (this[i]) return true;
+Array.prototype.myIncludes = function(search,index) {
+    var obj = search
+    for(let i = index || 0 ; i < this.length ; i++){
+       // if (this[i] == undefined) continue;
+        if (this[i] === obj) return true;
     }
     return false;
 };
 
 // // INDEXOF //
-// Array.prototype.myIndexOf = function() {
-
-// };
+Array.prototype.myIndexOf = function(arg,index) {
+    for(let i = index || 0 ; i < this.length; i++){
+        if (this[i] == undefined) continue;
+        if (this[i]=== arg) {return i;}
+    }
+    return -1;
+};
 
 // // PUSH //
-// Array.prototype.myPush = function() {
-
-// };
+Array.prototype.myPush = function(...arg) {
+    for(let i = 0 ; i < arg.length; i++){
+        if (this[i] == undefined) continue;
+        this[this.length]=(arg[i])
+    } 
+    return (this.length);
+};
 
 // // LASTINDEXOF //
-// Array.prototype.myLastIndexOf = function() {
+Array.prototype.myLastIndexOf = function(arg, index) {
+    for(let i = index || this.length - 1 ; i > -1; i--){
+        if (this[i] == undefined) continue;
+        if (this[i] === arg) {return i;}
+    }
+    return -1;
+};
 
-// };
-
-// // KEYS //
-// Object.grabKeys = function() {
-
-// };
+// // KEYS // 
+Object.grabKeys = function(object) {
+    var keys = [];
+    for (let i in object) {
+    keys[keys.length] = i;
+    }
+    return keys;
+};
 
 // // VALUES //
-// Object.grabValues = function() {
+Object.grabValues = function(object) {
+    var keys = [];
+    for (let i in object) {
+    keys[keys.length] = object[i];
+    }
+    return keys;
+};
 
-// };
